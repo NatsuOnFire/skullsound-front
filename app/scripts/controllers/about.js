a864a0f81e158fcfa3f8f3338c00fb36410d8eb4
Florian <laforest.florian@gmail.com>
2016-05-19 15:37:38 +0200

    Init project angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skullsoundApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the skullsoundApp
+ */
+angular.module('skullsoundApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skullsoundApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the skullsoundApp
+ */
+angular.module('skullsoundApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
